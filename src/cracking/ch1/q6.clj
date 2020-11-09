8cdd69f9de57f30d93a66584dc710292a3b5dc97
Travis Brown <4742936+reifying@users.noreply.github.com>
2020-11-08 19:41:38 -0600

    add test

---
+++ src/cracking/ch1/q6.clj
@@ -0,0 +1,7 @@
+(ns cracking.ch1.q6)
+
+(defn compressed
+  "Return the counts of each letter (e.g., aaabbc => a3b2c1) unless the result's
+   length excedes the original string's length"
+  [s]
+  s)
---
+++ test/cracking/ch1/q6_test.clj
@@ -0,0 +1,7 @@
+(ns cracking.ch1.q6-test
+  (:require [clojure.test :refer :all]
+            [cracking.ch1.q6 :refer :all]))
+
+(deftest string-compression
+  (is (= "a2b1c5a3" (compressed "aabcccccaaa")))
+  (is (= "aBc" (compressed "aBc"))))
