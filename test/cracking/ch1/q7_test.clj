c7ee740e211066f52228b08a7fbdd2fd0145c206
Travis Brown <4742936+reifying@users.noreply.github.com>
2020-11-09 21:33:30 -0600

    = does not work for java arrays

--- test/cracking/ch1/q7_test.clj
+++ test/cracking/ch1/q7_test.clj
@@ -2,10 +2,15 @@
   (:require [clojure.test :refer :all]
             [cracking.ch1.q7 :refer :all]))
 
+(defn- to-vec-2d [coll]
+  (into [] (map vec coll)))
+
 (deftest rotate-matrix
-  (is (= [[0 1 2]
-          [3 4 5]
-          [6 7 8]]
-         (rotate-90 [[6 3 0]
-                     [7 4 1]
-                     [8 5 2]]))))
+  (is (= [[6 3 0]
+          [7 4 1]
+          [8 5 2]]
+         (to-vec-2d
+          (rotate-90
+           (to-array-2d [[0 1 2]
+                         [3 4 5]
+                         [6 7 8]]))))))
