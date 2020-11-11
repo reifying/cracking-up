e369cd7d6d90b9e3b9a27b765d678cf94911e50d
Travis Brown <4742936+reifying@users.noreply.github.com>
2020-11-10 22:26:10 -0600

    solved

--- src/cracking/ch1/q9.clj
+++ src/cracking/ch1/q9.clj
@@ -1,4 +1,7 @@
 (ns cracking.ch1.q9)
 
-(defn string-rotation? [s1 s2]
-  false)
+(defn string-rotation?
+  "Is s2 a rotation of s1?"
+  [s1 s2]
+  (and (= (count s1) (count s2))
+       (clojure.string/includes? (str s2 s2) s1)))
