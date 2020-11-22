153960fabc063a8faee7eeca8b85c985b8a75a7a
Travis Brown <4742936+reifying@users.noreply.github.com>
2020-11-21 22:04:47 -0600

    because docs

--- src/gen_stub.clj
+++ src/gen_stub.clj
@@ -1,6 +1,8 @@
 (ns gen-stub)
 
-(defn -main [ch q]
+(defn -main
+  "Generate scaffolding src and test files for a given chapter and question"
+  [ch q]
   (let [src-ns (str "cracking.ch" ch ".q" q)]
     (spit (str "src/cracking/ch" ch "/q" q ".clj")
           (str "(ns " src-ns ")\n\n"))
@@ -8,4 +10,3 @@
           (str "(ns cracking.ch" ch ".q" q "-test
   (:require [clojure.test :refer :all]
             [" src-ns " :refer :all]))\n\n"))))
-
