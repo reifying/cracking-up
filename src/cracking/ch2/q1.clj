40c101c1175dda515e5232242d506840767b7318
Travis Brown <4742936+reifying@users.noreply.github.com>
2020-11-21 23:01:36 -0600

    green

--- src/cracking/ch2/q1.clj
+++ src/cracking/ch2/q1.clj
@@ -1,4 +1,9 @@
 (ns cracking.ch2.q1)
 
 (defn dedupe-list [xs]
-  xs)
+  (loop [visited #{} xs xs nodups '()]
+    (if (seq xs)
+      (if (visited (first xs))
+        (recur visited (rest xs) nodups)
+        (recur (conj visited (first xs)) (rest xs) (cons (first xs) nodups)))
+      (reverse nodups))))
